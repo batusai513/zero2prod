0d070e7f0394d640f8c854b9ab51067f4660cb13
Richard Roncancio <batusai513@msn.com>
2022-11-03 18:33:36 +0100

    feat: add subscriptions endpoint with tests

--- src/lib.rs
+++ src/lib.rs
@@ -4,14 +4,28 @@ use actix_web::{dev::Server, web, App, HttpResponse, HttpServer, Responder};
 
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     // Route::new().guard(guard::Get())
-    let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
-        // .bind(address)?
-        .listen(listener)?
-        .run();
+    let server = HttpServer::new(|| {
+        App::new()
+            .route("/health_check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscribe))
+    })
+    // .bind(address)?
+    .listen(listener)?
+    .run();
 
     Ok(server)
 }
 
+#[derive(serde::Deserialize)]
+struct FormData {
+    email: String,
+    name: String,
+}
+
 async fn health_check() -> impl Responder {
+    HttpResponse::Ok().finish()
+}
+
+async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
     HttpResponse::Ok().finish()
 }
