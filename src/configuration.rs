dae3b5a90e42f254a60cd30319d5f94df4893ca4
Richard Roncancio <batusai513@msn.com>
2022-11-05 13:27:03 +0100

    feat: load app configuration from yaml file

---
+++ src/configuration.rs
@@ -0,0 +1,38 @@
+#[derive(serde::Deserialize)]
+pub struct Settings {
+    pub application_port: u16,
+    pub database: DatabaseSettings,
+}
+
+#[derive(serde::Deserialize)]
+pub struct DatabaseSettings {
+    pub username: String,
+    pub password: String,
+    pub port: u16,
+    pub host: String,
+    pub database_name: String,
+}
+
+impl DatabaseSettings {
+    pub fn connection_string(&self) -> String {
+        format!(
+            "postgres://{}:{}@{}:{}/{}",
+            self.username, self.password, self.host, self.port, self.database_name
+        )
+    }
+}
+
+pub fn get_configuration() -> Result<Settings, config::ConfigError> {
+    //initialise the configuration builder
+    let settings = config::Config::builder()
+        //Add configuration values from a file named `configuration.yaml`
+        .add_source(config::File::new(
+            "configuration.yaml",
+            config::FileFormat::Yaml,
+        ))
+        .build()?;
+
+    //Try to convert the configuration values it read into our Setting type
+
+    settings.try_deserialize::<Settings>()
+}
