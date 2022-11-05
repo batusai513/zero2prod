c399b5ce974a82e003f0760d5fa7375688c831ad
Richard Roncancio <batusai513@msn.com>
2022-11-05 13:28:03 +0100

    chore: update tests to include daabase connection

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,5 +1,8 @@
 use std::net::TcpListener;
 
+use sqlx::{Connection, PgConnection};
+use zero2prod::configuration;
+
 #[tokio::test]
 async fn health_check_works() {
     let address = spawn_app();
@@ -19,7 +22,7 @@ async fn health_check_works() {
 #[tokio::test]
 async fn subscribe_returns_a_200_for_valid_form_data() {
     let app_address = spawn_app();
-
+    let configuration = configuration::get_configuration().expect("Error reading configuration");
     let client = reqwest::Client::new();
 
     let body = "name=le%20guin&email=ursula_le_guin%40@gmail.com";
@@ -32,6 +35,20 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
         .expect("Failed to execute request");
 
     assert_eq!(200, response.status().as_u16());
+    
+    let connection_string = configuration.database.connection_string();
+    // The connection trait mus be in scope for us to invoke
+    // PgConnection::connect it is not an inherent method of the struct
+
+    let mut connection = PgConnection::connect(&connection_string)
+        .await
+        .expect("Failed to connect to postgres");
+
+    let saved = sqlx::query!("SELECT email, name from subscriptions",)
+        .fetch_one(&mut connection)
+        .await
+        .expect("Failed to fetch saved subscription");
+
 }
 
 #[tokio::test]
